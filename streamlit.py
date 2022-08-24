5823a3664694c089df75182484112f3a2cdf3a43
danielgauthiermtgov <111998873+danielgauthiermtgov@users.noreply.github.com>
2022-08-24 09:30:13 -0600

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,7 @@
+import streamlit
+streamlit.title('My partents new Healthy diner')
 
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & rocket smoothy')
+streamlit.text('Hard-boiled Free-Range Egg')
